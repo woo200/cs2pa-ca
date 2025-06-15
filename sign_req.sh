7133606f8c0596a28465380b7aaff85601c986d3
Bandit <bandit@Johns-MacBook-Pro.local>
2025-06-14 20:33:59 -0700

    Added request signing for normal CRLs

---
+++ sign_req.sh
@@ -0,0 +1,24 @@
+function print_usage() {
+    echo "Usage: $0 [request.csr] [output_certificate.pem]"
+    printf "%${#0}s"
+    echo "        -h (Help on generating certs and requests)"
+    
+}
+
+if [[ $1 == "-h" ]]
+then
+    echo "openssl req -addext \"subjectAltName = subjectAltName=DNS.1:domain.com,DNS.2:domain.com,IP:127.0.0.1\" \\"
+    echo "            -newkey rsa:2048 -keyout PRIVATEKEY.key -nodes -out MYCSR.csr"
+    exit
+fi
+
+if [ $# -lt 2 ]
+then
+    print_usage
+    exit
+fi
+
+openssl req -in $1 -text -noout
+openssl ca -config openssl.cnf \
+    -out $2 \
+    -in $1 
